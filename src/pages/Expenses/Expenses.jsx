4bd74138f50646a70385540b2e759e2d55d255d1
Edward Shanahan <312564962+edwardshanahan97@users.noreply.github.com>
2026-09-14 17:07:28 +0100

    feat: add pagination to income and expenses

--- src/pages/Expenses/Expenses.jsx
+++ src/pages/Expenses/Expenses.jsx
@@ -3,19 +3,39 @@ import { useEffect } from "react";
 import { usePage } from "../../context/PageContext";
 import Button from "../../components/Button/Button";
 import AddTransaction from "../../components/AddTransaction/AddTransaction";
+import Transactions from "../../components/Transactions/Transactions";
+import { useFinance } from "../../context/FinanceContext";
+import usePagination from "../../hooks/usePagination";
+import Pagination from "../../components/Pagination/Pagination";
 
 const Expenses = ({ isActive, setIsActive }) => {
   const { setCurrentPage } = usePage();
+  const { finance } = useFinance();
+  const expensesTransactions = finance.transactions.filter(
+    (transaction) => transaction.type === "expenses",
+  );
+  const { page, setPage, totalPages, currentItems } =
+    usePagination(expensesTransactions);
+
+  const recentTransaction = expensesTransactions
+    .slice(-5)
+    .sort((a, b) => b.date.localeCompare(a.date));
 
   useEffect(() => setCurrentPage("expenses"), []);
-
   return (
     <main>
       <AddTransaction isActive={isActive} setIsActive={setIsActive} />
+
+      <Transactions title="Recent Expenses" transactions={recentTransaction} />
+
+      <Transactions title="All Expenses" transactions={currentItems} />
+
+      <Pagination page={page} setPage={setPage} totalPages={totalPages} />
+
       <Button
         className="button--fixed"
         isMobile={true}
-        title="Add Expense"
+        title="Add Expenses"
         isActive={isActive}
         setIsActive={setIsActive}
       />
--- src/pages/Income/Income.jsx
+++ src/pages/Income/Income.jsx
@@ -3,14 +3,35 @@ import { useEffect } from "react";
 import { usePage } from "../../context/PageContext";
 import Button from "../../components/Button/Button";
 import AddTransaction from "../../components/AddTransaction/AddTransaction";
+import Transactions from "../../components/Transactions/Transactions";
+import { useFinance } from "../../context/FinanceContext";
+import usePagination from "../../hooks/usePagination";
+import Pagination from "../../components/Pagination/Pagination";
 
 const Income = ({ isActive, setIsActive }) => {
   const { setCurrentPage } = usePage();
+  const { finance } = useFinance();
+  const incomeTransactions = finance.transactions.filter(
+    (transaction) => transaction.type === "income",
+  );
+  const { page, setPage, totalPages, currentItems } =
+    usePagination(incomeTransactions);
+
+  const recentTransaction = incomeTransactions
+    .slice(-5)
+    .sort((a, b) => b.date.localeCompare(a.date));
 
   useEffect(() => setCurrentPage("income"), []);
   return (
     <main>
       <AddTransaction isActive={isActive} setIsActive={setIsActive} />
+
+      <Transactions title="Recent Income" transactions={recentTransaction} />
+
+      <Transactions title="All Income" transactions={currentItems} />
+
+      <Pagination page={page} setPage={setPage} totalPages={totalPages} />
+
       <Button
         className="button--fixed"
         isMobile={true}
